62c667e5673e2f89ba6f65f9a1b8ba4d3b9dc2b6
Moa Ziegler <moa.ziegler@infarm.com>
2022-07-28 21:32:21 +0200

    Fixed versions and enables release script again

--- release.sh
+++ release.sh
@@ -23,11 +23,10 @@ if [[ -n "$(git tag | grep "${VERSION}" || true)" ]]; then
 fi
 
 OLD_VERSION=$(git describe --tags --abbrev=0)
-sed -i "s/\"version\":.*/\"version\": \"${VERSION}\",/" package.json
-sed -i "s/\"version\":.*/\"version\": \"${VERSION}\",/" package-lock.json
-sed -i "s/${OLD_VERSION}/${VERSION}/" wled00/*.h wled00/*.cpp
-#git add -u -m "Updated version in package.json to ${VERSION}"
-#git tag -a "${VERSION}" -m "Created tag ${VERSION}"
+sed -i "s/${OLD_VERSION}/${VERSION}/" wled00/*.h wled00/*.cpp *.json
+git add -u
+git commit -m "Updated version in package.json to ${VERSION}"
+git tag -a "${VERSION}" -m "Created tag ${VERSION}"
 
 echo "INFO: If everything looks good push the release with the following command, so the firmware will be build"
 echo "CMD: git push -u origin main --tags"
